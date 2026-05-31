667eaf68044ce7fd608d1d6d7c9d54ad9c33fc78
Prabhakar Perugu <prabhakarp2969@gmail.com>
2026-05-31 14:17:08 +0530

    initial commit

---
+++ src/webparts/selfAppraisalPortal/components/SelfAppraisalPortal/ISelfAppraisalPortal.ts
@@ -0,0 +1,7 @@
+export interface ISelfAppraisalPortalProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
